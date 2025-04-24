78f48cb67bca090392ea6212724ee4b83b549179
yuvaraj k <113287187+yuvrajyuvi@users.noreply.github.com>
2025-04-24 23:17:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-}
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+    const charcount={}
+
+    for(let char of str ){
+        charcount[char]=(charcount[char]||0)+1;
+    }
+    for(let char of str){
+        if(charcount[char]===1){
+            return char;
+        }
+        else{
+            return null;
+        }
+    }
+
+   }
+   const input = prompt("Enter a string");
+   alert(firstNonRepeatedChar(input)); 
+   
