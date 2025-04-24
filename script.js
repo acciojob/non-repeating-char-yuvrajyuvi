d2ea46d3000c66f8f7aa9e0a94db8962f127feae
yuvaraj k <113287187+yuvrajyuvi@users.noreply.github.com>
2025-04-24 23:47:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function firstNonRepeatedChar(str) {
-    const charcount={}
-	if(str.length===0){
-        return null;
+    let charcount={}
+    if(str.length===0){
+        return null
     }
+
     for(let char of str ){
-        charcount[char]=(charcount[char]||0)+1;
+        if(charcount[char]){
+            charcount[char]++;
+        }
+        else{
+            charcount[char]=1;
+        }
     }
     for(let char of str){
         if(charcount[char]===1){
             return char;
         }
-        else{
-            return null;
-        }
-    }
-	return null;
-
-   }
+	}
+	return null
+		
+ }
    const input = prompt("Enter a string");
    alert(firstNonRepeatedChar(input)); 
-   
